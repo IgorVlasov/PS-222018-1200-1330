d03b69bfd22c19ec5bb8eca7e9591c39d1a09400
buuuusyyyy <gopop85724755@gmail.com>
2021-02-21 10:22:51 +0000

    Добавил несколько объектов

--- todo.py
+++ todo.py
@@ -1,10 +1,11 @@
-HELP = """
-help      -
-add       -
-remove    - 
-show      -
-exit      -
+import time
 
+HELP = """
+help   - показывает список команд
+add    - добавить
+remove - удалить
+show   - показать
+exit   - выход
 """
 
 
@@ -12,18 +13,34 @@ todo = {}
 userAnswer = ""
 
 while True:
-  userAnswer = input()
+  userAnswer = input().lower()
 
   if userAnswer == "help":
     print(HELP)
   elif userAnswer == "add":
-    print("Задача добалена")
+    print("Введите дату выполнения в формате mm.dd.yyyy")
+    uDate = input()
+    try:
+      valid_date = time.strptime(uDate, '%m.%d.%Y')
+    except ValueError:
+      print('Неверно введена дата!')
+      continue
+      
+    print("Введите задачу")
+    uTask = input()
+    if uDate in todo:
+      todo[uDate].append(uTask)
+    else:
+      todo[uDate] = [uTask]
+    print(f"Задача {uTask} добалена\n")
   elif userAnswer == "remove":
-    print("Задача удалена")
+    print("Задача удалена\n")
   elif userAnswer == "show":
-    print("Задача показана")
+    for i in sorted( todo.keys() )  
+      for i in todo.keys():
+        print(f"[{i}]\t {todo[i]} ")
   elif userAnswer == "exit":
-    print("Завершение!")
+    print("Завершение!\n")
     break
   else:
     print("Вы ввели неизвестную задачу")
