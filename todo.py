930953aeefd58b5fbe8c543e5ee24aa71d539957
buuuusyyyy <gopop85724755@gmail.com>
2021-02-28 10:26:18 +0000

    Добавил команду showall и соответственно изменил команду show

--- todo.py
+++ todo.py
@@ -1,15 +1,33 @@
 import time
 
 HELP = """
-help   - показывает список команд
-add    - добавить
-remove - удалить
-show   - показать
-exit   - выход
+help      - показывает список команд
+add       - добавить
+remove    - удалить
+showall   - показать все задачи
+show      - показать задачи по указанной дате
+exit      - выход
 """
 
+def checkTime(Date):
+  try:
+    time.strptime(Date, '%m.%d.%Y')
+    return True
+  except ValueError:
+    print('Неверно введена дата!')
+    return False
 
-todo = {}
+def addTask(Date, Task):
+  if Date in todo:
+      todo[Date].append(Task)
+  else:
+      todo[Date] = [Task]
+  print(f"Задача {Task} добалена\n")
+
+todo = {
+  "12.12.2021":["Тестовая задача 1", "Тестовая задача 2"],
+  "11.11.2021":["Тестовая задача 3"],
+}
 userAnswer = ""
 
 while True:
@@ -17,31 +35,40 @@ while True:
 
   if userAnswer == "help":
     print(HELP)
+
+
   elif userAnswer == "add":
-    print("Введите дату выполнения в формате mm.dd.yyyy")
-    uDate = input()
-    try:
-      valid_date = time.strptime(uDate, '%m.%d.%Y')
-    except ValueError:
-      print('Неверно введена дата!')
+    uDate = input("Введите дату выполнения в формате mm.dd.yyyy\n")
+    if checkTime(uDate) == False:
       continue
-      
-    print("Введите задачу")
-    uTask = input()
-    if uDate in todo:
-      todo[uDate].append(uTask)
-    else:
-      todo[uDate] = [uTask]
-    print(f"Задача {uTask} добалена\n")
+    uTask = input("Введите задачу\n")
+    addTask(uDate, uTask)
+
+
   elif userAnswer == "remove":
     print("Задача удалена\n")
+
+
+  elif userAnswer == "showall":
+    for i in sorted(todo.keys()):
+      for j in todo[i]:
+        print(f"[{i}]\t {j}")
+
+
   elif userAnswer == "show":
-    for i in sorted( todo.keys() )  
-      for i in todo.keys():
-        print(f"[{i}]\t {todo[i]} ")
+    uInput = input("Введите дату выполнения в формате mm.dd.yyyy\n")
+    if uInput in todo.keys():
+      for i in todo[uInput]:
+        print(f"[{uInput}]\t {i}")
+    elif checkTime(uInput) == True:
+      print("Нет задач по указанной дате")
+
+
   elif userAnswer == "exit":
     print("Завершение!\n")
     break
+
+    
   else:
     print("Вы ввели неизвестную задачу")
     print("Для вывода списка команд введите help")
