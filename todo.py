f1db4ad4dca0c49983866a2739f99c6bf6d08db7
Jeeenkens <bunarev@inbox.ru>
2021-02-28 10:28:27 +0000

    show

--- todo.py
+++ todo.py
@@ -1,14 +1,33 @@
 import time
 
 HELP = """
-help    - список команд
-add     - добавить задачу
-remove  - удалить задачу
-show    - показать задачи
-exit    - закрыть программу
+help     - список команд
+add      - добавить задачу
+remove   - удалить задачу
+showall  - показать задачи
+show     - показать задачи по дате
+exit     - закрыть программу
 """
 
-todo = {}
+def checkTime(Date):
+  try:
+    time.strptime(Date, '%m.%d.%Y')
+    return True
+  except ValueError:
+    print('Invalid date!')
+    return False
+
+def addTask(Date, Task):
+  if Date in todo:
+    todo[Date].append(Task)
+  else:
+    todo[Date] = [Task]
+  print(f"задача {Task} добавлена\n" )
+
+todo = {
+  "12.12.2021":["Do 1", "Do 2"],
+  "12.02.2021":["Do 3"]
+}
 userAnswer = ""
 print("Для вывода списка команд введите help")
 
@@ -18,27 +37,27 @@ while True:
   if userAnswer == "help":
     print( HELP )
   elif userAnswer == "add":
-    print("Введите дату в формате mm.dd.yyyy")
-    uDate = input()
-    try:
-      valid_date = time.strptime(uDate, '%m./%d./%Y')
-    except ValueError:
-      print('Invalid date!')
+    uDate = input("Введите дату в формате mm.dd.yyyy\n")
+    if checkTime(uDate) == False:
       continue
 
-    print("Введите задачу")
-    uTask = input()
-    if uDate in todo:
-      todo[uDate].append(uTask)
-    else:
-      todo[uDate] = [uTask]
-    print(f"задача {uTask} добавлена\n" )
+    uTask = input("Введите задачу\n")
+    addTask(uDate, uTask)
+
   elif userAnswer == "remove":
     print( "задача удалена\n" )
-  elif userAnswer == "show":
+  elif userAnswer == "showall":
     for i in sorted(todo.keys() ):
       for j in todo[i]:
         print( f"[{i}]\t {j} " )
+  
+  elif userAnswer == "show":
+    uDate = input("Какую дату вы хотите посмотреть?\n")
+    for i in sorted(todo.keys() ):
+      if i==uDate:
+        for j in todo[i]:
+          print( f"[{i}]\t {j} " )
+
   elif userAnswer == "exit":
     print( "До свидания!\n" )
     break
